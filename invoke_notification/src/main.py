290e79a4cf3a044eff4e17d57fae42844d54b9a0
rafikee <rafik.vcu@gmail.com>
2025-07-31 17:26:05 -0700

    Add invoke_notification function and remove testfunction

--- invoke_notification/src/main.py
+++ invoke_notification/src/main.py
@@ -32,7 +32,7 @@ def main(context):
 
     return context.res.json(
         {
-            "motto": "Build like a team of hundreds_",
+            "motto": "Rafik is soo cooool",
             "learn": "https://appwrite.io/docs",
             "connect": "https://appwrite.io/discord",
             "getInspired": "https://builtwith.appwrite.io",
