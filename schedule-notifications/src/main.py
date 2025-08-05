110f377f5bc220139317facd806b3989f0aed9af
rafikee <rafik.vcu@gmail.com>
2025-08-05 06:50:59 -0700

    Update appwrite config and schedule-notifications function

--- schedule-notifications/src/main.py
+++ schedule-notifications/src/main.py
@@ -1,41 +1,26 @@
 from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
+from appwrite.services.databases import Databases
 import os
 
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
+    # Set project and set API key
     client = (
         Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
         .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
         .set_key(context.req.headers["x-appwrite-key"])
     )
-    users = Users(client)
+
+    databases = Databases(client)
 
     try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
+        result = databases.list_collections(
+            database_id="projectbilal",
+        )
+        context.log(result)
+    except Exception as e:
+        context.error("Failed to create document: " + e.message)
+        return context.res.text("Failed to create document")
 
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Syria is soo cooool",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-            "hello": "world",
-        }
-    )
+    return context.res.text("Complete")
